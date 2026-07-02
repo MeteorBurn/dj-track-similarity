6d02727ade9ff99df6b47a00e56ead7328fc0eec
root <root@aet-intel-nl-3-6391142-446487-zomro.vps.ac>
2026-07-02 03:13:41 +0000

    docs: add local font stack for site

--- docs/dj-track-similarity/.vitepress/theme/index.ts
+++ docs/dj-track-similarity/.vitepress/theme/index.ts
@@ -1,4 +1,9 @@
 import DefaultTheme from "vitepress/theme";
+import "@fontsource-variable/ibm-plex-sans/wght.css";
+import "@fontsource-variable/ibm-plex-sans/wght-italic.css";
+import "@fontsource-variable/space-grotesk/wght.css";
+import "@fontsource-variable/jetbrains-mono/wght.css";
+import "@fontsource-variable/jetbrains-mono/wght-italic.css";
 import "./custom.css";
 
 export default DefaultTheme;

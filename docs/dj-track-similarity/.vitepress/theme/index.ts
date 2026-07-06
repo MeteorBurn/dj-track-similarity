a36fbb255c9f80fa478e43691cb9d8eeedbdb8b9
Meteorburn <MeteorBurn@users.noreply.github.com>
2026-07-07 02:58:11 +0300

    Polish documentation site layout

--- docs/dj-track-similarity/.vitepress/theme/index.ts
+++ docs/dj-track-similarity/.vitepress/theme/index.ts
@@ -1,7 +1,4 @@
 import DefaultTheme from "vitepress/theme";
-import "@fontsource-variable/ibm-plex-sans/wght.css";
-import "@fontsource-variable/ibm-plex-sans/wght-italic.css";
-import "@fontsource-variable/space-grotesk/wght.css";
 import "@fontsource-variable/jetbrains-mono/wght.css";
 import "@fontsource-variable/jetbrains-mono/wght-italic.css";
 import "./custom.css";
